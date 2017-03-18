6a4557362e8a1d617d46774ee135b1da507f2c5f
Ryan T. Hosford <tad.hosford@gmail.com>
2017-03-18 15:56:31 -0500

    Remove final TODO

--- bin/runner.rb
+++ bin/runner.rb
@@ -3,5 +3,4 @@ require_relative "../lib/prog/channels"
 @slack_client     = Slack::Web::Client.new(token: ENV['SLACK_API_TOKEN'])
 Airrecord.api_key = ENV['AIRTABLE_KEY']
 
-# TODO, Run me daily:
 Prog::Channels::Syncopator.new(slack_client: @slack_client, table: Prog::Channels::ChannelList).call
