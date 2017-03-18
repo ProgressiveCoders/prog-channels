414d6173056d69189e4e57db88ced3b6d40f39c2
Ryan T. Hosford <tad.hosford@gmail.com>
2017-03-18 14:33:06 -0500

    Get ready for a small heroku deployment
      - Config.ru
      - Add Gemfile.lock to please heroku. Omit it from gem, remove it from gitignore
      - Add rack
      - Rackup
      - Start on certain port
      - ENV driven choice of table name
      - Trim down runner

--- bin/runner.rb
+++ bin/runner.rb
@@ -1,17 +1,3 @@
-begin
-  require "bundler/inline"
-rescue LoadError => e
-  $stderr.puts "Bundler version 1.10 or later is required. Please update your Bundler"
-  raise e
-end
-
-gemfile(true) do
-  source "https://rubygems.org"
-
-  gem "prog-channels", path: "."
-end
-
-require "minitest/autorun"
 require_relative "../lib/prog/channels"
 
 @slack_client     = Slack::Web::Client.new(token: ENV['SLACK_API_TOKEN'])
